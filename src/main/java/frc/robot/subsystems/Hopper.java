574541808682bc61cb9ff695c773acd0abc648ca
Andrew Galbraith Ryer <drewgr@verizon.net>
2022-01-14 20:57:18 -0500

    Updated Hopper to use VictorSPX, removed Pixy

--- src/main/java/frc/robot/subsystems/Hopper.java
+++ src/main/java/frc/robot/subsystems/Hopper.java
@@ -7,48 +7,45 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import frc.robot.RobotMap.CAN;
-import frc.robot.sensors.BallCounter;
 
-import frc.robot.util.MercMotorController.*;
-import frc.robot.util.interfaces.IMercMotorController;
 import frc.robot.util.interfaces.IMercShuffleBoardPublisher;
 
 public class Hopper extends SubsystemBase implements IMercShuffleBoardPublisher {
   
-  private IMercMotorController hopperBelt;
+  private VictorSPX hopperBelt;
   private final double RUN_SPEED;
-  private BallCounter ballCounter;
+
   /**
    * Creates a new Hopper.
    */
   public Hopper() {
     RUN_SPEED = -0.5;
 
-    hopperBelt = new MercVictorSPX(CAN.HOPPER_BELT);
+    hopperBelt = new VictorSPX(CAN.HOPPER_BELT);
     hopperBelt.setNeutralMode(NeutralMode.Brake);
 
     setName("Hopper");
-
-    ballCounter = new BallCounter();
   }
 
   public void setSpeed(double speed) {
-    hopperBelt.setSpeed(speed);
+    hopperBelt.set(ControlMode.PercentOutput, speed);
   }
 
   public void stopHopper() {
-    hopperBelt.setSpeed(0.0);
+    setSpeed(0.0);
   }
 
   public void runHopper(){
-    hopperBelt.setSpeed(RUN_SPEED);
+    setSpeed(RUN_SPEED);
   }
 
   public double getRunSpeed() {
@@ -56,17 +53,7 @@ public class Hopper extends SubsystemBase implements IMercShuffleBoardPublisher 
   }
 
   @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-  }
-
-  @Override
   public void publishValues() {
-    SmartDashboard.putNumber(getName() + "/BallCount", ballCounter.getCount());
-    SmartDashboard.putNumber(getName() + "/Sig1", ballCounter.pixyCam.getBoxes(1).size());
-    SmartDashboard.putNumber(getName() + "/Sig2", ballCounter.pixyCam.getBoxes(2).size());
-    SmartDashboard.putNumber(getName() + "/Sig3", ballCounter.pixyCam.getBoxes(3).size());
-    SmartDashboard.putNumber(getName() + "/Sig4", ballCounter.pixyCam.getBoxes(4).size());
-    SmartDashboard.putNumber(getName() + "/Sig5", ballCounter.pixyCam.getBoxes(5).size());
+
   }
 }
