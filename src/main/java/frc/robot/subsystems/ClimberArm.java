de4a88b8e62ad009bdd8122fc8f2979b4804497a
Mercury 1089 <1089software@gmail.com>
2022-03-16 20:54:54 -0400

    created climber subsystems

--- src/main/java/frc/robot/subsystems/ClimberArm.java
+++ src/main/java/frc/robot/subsystems/ClimberArm.java
@@ -1,23 +1,48 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.util.interfaces.IMercShuffleBoardPublisher;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
 import frc.robot.RobotMap.CAN;
 
 public class ClimberArm extends SubsystemBase {
-    private final TalonSRX climberArm;
-    private IntakeArticulator intakeArticulator;
-    private final double OUT_SPEED = 0.5, IN_SPEED = -0.6;
-
-
+    private final TalonSRX climberArmArticulator;
+    private ClimberPosition climberArmPosition;
+    private final double BACKWARD_SPEED = -0.5, FORWARD_SPEED = 0.5;
     
-    public ClimberArm() {
+    public ClimberArm() { //unsure about this constructor
         super();
-        climberArm = new TalonSRX(CAN.INTAKE_ARTICULATOR);
+        climberArmArticulator = new TalonSRX(CAN.CLIMBER_ARM_ARTICULATOR); 
+        climberArmArticulator.configFactoryDefault();                 
+        climberArmArticulator.setNeutralMode(NeutralMode.Brake);
+        climberArmArticulator.setInverted(true);
+        climberArmPosition = ClimberPosition.BACKWARD;                  
     }
-    public void setClimberArmForward() {
 
+    public enum ClimberPosition{
+        FORWARD,
+        BACKWARD,
+        DISABLED
+    }
+    
+    public void setClimberArmForward() {
+        this.climberArmPosition = ClimberPosition.FORWARD;
+        climberArmArticulator.set(ControlMode.PercentOutput, FORWARD_SPEED);
+    }
+    
+    public void setClimberArmBackward() {
+        this.climberArmPosition = ClimberPosition.BACKWARD;
+        climberArmArticulator.set(ControlMode.PercentOutput, BACKWARD_SPEED);
+    }
+    
+    public void setClimberArmDisabled() {
+        this.climberArmPosition = ClimberPosition.DISABLED;
+        climberArmArticulator.set(ControlMode.PercentOutput, 0.0);
+    }
+    
+    public ClimberPosition getClimberPosition() {
+        return this.climberArmPosition;
     }
 
 }
--- src/main/java/frc/robot/subsystems/ClimberWinch.java
+++ src/main/java/frc/robot/subsystems/ClimberWinch.java
@@ -1,5 +1,40 @@
 package frc.robot.subsystems;
 
-public class ClimberWinch {
-    
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import frc.robot.RobotMap.CAN;
+
+public class ClimberWinch extends SubsystemBase{
+    private final TalonFX climberWinchArticulator;
+    private ClimberWinchStatus climberWinch;
+    private final double CLIMBER_WINCH_SPEED = 0.5;
+
+    public ClimberWinch() {
+        super();
+        climberWinchArticulator = new TalonFX(CAN.CLIMBER_WINCH);
+        climberWinch = ClimberWinchStatus.DISABLED;
+    }
+
+    public enum ClimberWinchStatus {
+        ACTIVE,
+        DISABLED
+    }
+
+    public void setClimberWinchSpeedActive() {
+        this.climberWinch = ClimberWinchStatus.ACTIVE;
+        climberWinchArticulator.set(ControlMode.PercentOutput,CLIMBER_WINCH_SPEED);
+    }
+
+    public void setClimberWinchSpeedDisabled() {
+        this.climberWinch = ClimberWinchStatus.DISABLED;
+        climberWinchArticulator.set(ControlMode.PercentOutput,0.0);
+    }
+
+    public ClimberWinchStatus getClimberWinchSpeed() {
+        return this.climberWinch; //should be "return CLIMBER_WINCH_SPEED;"????? 
+    }
+
 }
+
